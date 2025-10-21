31999a5e07fae3c27cc675c56fd11b40ca2c745e
ENDERSON-MARIN <marinenderson1@gmail.com>
2025-10-21 11:50:09 -0300

    feat(server): add router and error handler updated

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import express from "express"
 import cors from "cors"
+import { router } from "./adapters/routes"
+import { errorHandler } from "./adapters/middlewares/errorHandler"
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
+
+app.use(router)
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`)
